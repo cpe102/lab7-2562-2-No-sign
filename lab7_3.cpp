fb5d511609e427c657e17ba060b34cfded76fcd3
No-sign <55203083+No-sign@users.noreply.github.com>
2020-01-27 19:24:07 +0700

    Add files via upload

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,1 +1,60 @@
-//status = "ONE-NIGHT-STAND";
+#include<iostream>
+#include<iomanip>
+#include<string>
+using namespace std;
+int age,high;
+long asset;
+string status;
+int main(){
+	cout<<"Input age:";
+	cin>>age;
+	while(1){
+	
+	if(age<=20){
+		cin.ignore();
+		cout<<"Input high:";
+		cin>>high;
+		if(high<160){
+			status="UNFRIEND";
+		break;
+		}
+		else if(high<175){
+			status="FRIEND";
+			break;
+		}
+		else{
+		cin.ignore();
+		cout<<"Input asset:";
+		cin>>asset;
+		if(asset>69000000){
+			status="MARRIED";
+			break;
+		}
+		else{
+			status="ONE-NIGHT-STAND";
+			break;
+		}
+		}
+	}
+	else if(age<30){
+		cin.ignore();
+		cout<<"Input asset:";
+		cin>>asset;
+		if(asset>10000000){
+			status="BEST FRIEND";
+			break;
+		}
+		else{
+			status="UNFRIEND";
+		}
+	}
+	else{
+		status="UNFRIEND";
+		break;
+	}
+	
+	break;
+}
+	cout<<status;
+	return 0;
+}
